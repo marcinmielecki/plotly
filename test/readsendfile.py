5439914e1cd7fa56c54a8d3862c4035ed231e922
kamgorhub <gorolkamil@gmail.com>
2015-11-05 10:16:25 +0100

    Read file test #1

--- test/readsendfile.py
+++ test/readsendfile.py
@@ -1,1 +1,11 @@
+import cx_Oracle
+
+file_T = open('C:/Users/Kamil/Documents/GitHub/infpro-9/T439_e-e-_w2-w2-_th13_SR.txt','r',encoding='UTF-8')
+for i in range(0,10):
+    print (file_T.readline())
+
+print('############################## \n')
+
+for line in file_T:
+    print(line)
 
