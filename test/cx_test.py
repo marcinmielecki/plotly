8ae32b359acba33f0b6b44a33c73a717e108155c
kamgorhub <gorolkamil@gmail.com>
2015-11-25 19:38:03 +0100

    simple inserting

--- test/cx_test.py
+++ test/cx_test.py
@@ -7,9 +7,20 @@ cursor = connection.cursor()
 print(connection.version)
 print(connection.dsn)
 
-cursor.execute('select * from newton')
+cursor.execute('select * from testowa')
 for result in cursor:
-    print(result)
+  print(result)
+#cursor.close()
+'''
+j=(5,6,6,6,7)
+k=(6,6,6,6,6)
+n=len(j)
+sql="insert into testowa (nazwa,n,k) values ('cx_oracle',:test,:test2)"
+
+for i in range(0,n):
+  cursor.execute(sql, test=j[i], test2=k[i])
+
+cursor.execute('commit')'''
 cursor.close()
 connection.close()
 
